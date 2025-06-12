dab740af0a81ee0b003f3d3a2740c431f3f605df
Cheonyeoun <vigneshpandiyan08@gmail.com>
2025-06-12 21:09:02 +0530

    Completed the assignment

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const express = require('express');
 const { resolve } = require('path');
+const Profile = require('./models/Profile');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const app = express();
+app.use(express.json());
 const port = 3010;
+const MONGO_URI = "mongodb+srv://vigneshpan08:vigneshpan08@cluster0.2pnpynh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
-app.use(express.static('static'));
+mongoose
+.connect(MONGO_URI)
+.then(()=>{
+    console.log("✔️ mongoDB connection successful!")
+})
+.catch((err)=> console.error("❌ Connection Failed",err))
+
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+  res.send({message:"Welcome to the Server!"});
+
 });
+
+app.post('/profile', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new Profile({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    return res.status(201).json({ success: true, message: 'Profile Creation Successful!' });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: 'Server error', err });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ models/Profile.js
@@ -0,0 +1,11 @@
+
+const mongoose = require('mongoose')
+
+const profileSchema = new mongoose.Schema({
+    username:{type:String, required:true},
+    email :{type:String, required:true},
+    password:{type:String,required:true}
+})
+
+const Profile = mongoose.model('Profile', profileSchema);
+module.exports = Profile;
